dd4035e3feaeb492e6a258b2bbe93564931000ec
BurramsettySushmaSri <sushmasri7174@gmail.com>
2022-02-25 12:07:51 +0530

    sushma change 12:07

--- lib/common/nav.dart
+++ lib/common/nav.dart
@@ -24,7 +24,7 @@ class _HomeWidgetState extends State<HomeWidget> {
         body: Row(
       children: [
         NavigationRail(
-     backgroundColor: Colors.red,
+     backgroundColor: Colors.blue,
           onDestinationSelected: (index) {
             setState(() {
               _selectedIndex = index;
